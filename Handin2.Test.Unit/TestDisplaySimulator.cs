a63a1b2caf1050f9585b544a6220ca551ae0f2d6
Victor Kildahl Petersen <au600963@uni.au.dk>
2020-03-18 15:31:55 +0100

    display tests

---
+++ Handin2.Test.Unit/TestDisplaySimulator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Handin2.Test.Unit
+{
+    [TestClass]
+    public class TestDisplaySimulator
+    {
+        private DisplaySimulator _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new DisplaySimulator();
+        }
+
+        [Test]
+        public void TestOpeningAClosedDoor()
+        {
+            _uut.print("hej");
+            Assert.That(_uut.test, Is.EqualTo(1));
+        }
+    }
+}
+
+
+
+
+
