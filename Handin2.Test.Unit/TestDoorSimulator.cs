adf3b551c99aca9b8226999123b588735cbe4229
Tegam123 <47597872+Tegam123@users.noreply.github.com>
2020-03-18 12:58:23 +0100

    Coverage og test's

---
+++ Handin2.Test.Unit/TestDoorSimulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Handin2.Test.Unit
+{
+    [TestClass]
+    public class TestDoorSimulator
+    {
+
+        private DoorSimulator _uut;
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- Handin2.Test.Unit/TestRfidReaderSimulator.cs
+++ Handin2.Test.Unit/TestRfidReaderSimulator.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Handin2.RfidReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace Handin2.Test.Unit
-{
+{   
+    [TestClass]
     class TestRfidReaderSimulator
     {
         private RfidReaderSimulator _uut;
@@ -18,6 +21,29 @@ namespace Handin2.Test.Unit
         {
             _recievedRfidEventArgs = null;
             _uut = new RfidReaderSimulator();
+
+            _uut.RfidEvent += (o, args) =>
+            {
+                _recievedRfidEventArgs = args;
+            };
         }
+
+        [Test]
+        public void TestRfidRaiseEvent()
+        {
+            _uut.OnRfidRead(1234);
+            Assert.That(_recievedRfidEventArgs, Is.Not.Null);
+        }
+
+        [Test]
+        public void TestRfidReaderRegisterGivenId()
+        {
+            _uut.OnRfidRead(1234);
+
+            Assert.That(_recievedRfidEventArgs.RfidTag,
+                Is.EqualTo(1234));
+
+        }
+
     }
 }
