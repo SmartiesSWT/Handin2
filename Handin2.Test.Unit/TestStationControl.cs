8cdd1f8a0e3ea60cc9063a8a89b38cc38fa3e366
Victor Kildahl Petersen <au600963@uni.au.dk>
2020-03-18 16:41:10 +0100

    små rettelser

--- Handin2.Test.Unit/TestStationControl.cs
+++ Handin2.Test.Unit/TestStationControl.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using NSubstitute;
 using Assert = NUnit.Framework.Assert;
 
 namespace Handin2.Test.Unit
@@ -12,11 +13,23 @@ namespace Handin2.Test.Unit
     [TestClass]
     public class TestStationControl
     {
-        [SetUp]
+        private StationControl _uut;
+        private IDoor _door;
+        private IDisplay _display;
+        private IRfidReader _rfid;
+        private IUsbCharger _usb;
 
+        [SetUp]
         public void Setup()
         {
-
+            _uut = new StationControl(_door, _usb, _display, _rfid);
+            _door = Substitute.For<IDoor>();
+            _display = Substitute.For<IDisplay>();
+            _rfid = Substitute.For<IRfidReader>();
+            _usb = Substitute.For<IUsbCharger>();
         }
+
+
+
     }
 }
